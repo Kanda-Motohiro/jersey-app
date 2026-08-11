a3c36476267de231b6cfc8b201774177628bfdce
Kanda Motohiro <kanda.motohiro@gmail.com>
2026-08-11 15:51:16 +0900

    add parm name to Hello

--- src/main/java/com/example/HelloResource.java
+++ src/main/java/com/example/HelloResource.java
@@ -3,13 +3,15 @@ package com.example;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("hello")
 public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello Jetty + Jersey";
+    public String hello(@QueryParam("name") @DefaultValue("Jetty + Jersey") String name) {
+        return "Hello " + name;
     }
 }
